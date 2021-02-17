c07306e1f14a0eb60ae92e6e0dc330e23a108e8a
Wallerand Delevacq <wallforfry@gmail.com>
2021-02-17 18:36:24 +0100

    Document LocalStorageToken

--- src/Token/LocalStorageToken.ts
+++ src/Token/LocalStorageToken.ts
@@ -1,26 +1,52 @@
 import Token from './Token'
 
+/**
+ * @class
+ * LocalStorageToken implementation
+ */
 class LocalStorageToken extends Token {
+  /**
+   * Set the Access Token from storage
+   * @param token - AccessToken as string
+   */
   setAccessToken(token: string): void {
     localStorage.setItem(this.getAccessTokenName(), token)
   }
 
+  /**
+   * Get the Access Token from storage
+   * @returns {string|undefined} The stored Access Token
+   */
   getAccessToken(): string | undefined {
     return localStorage.getItem(this.getAccessTokenName()) ?? undefined
   }
 
+  /**
+   * Remove the Access Token from storage
+   */
   unsetAccessToken(): void {
     localStorage.removeItem(this.getAccessTokenName())
   }
 
+  /**
+   * Set the Refresh Token from storage
+   * @param refreshToken - RefreshToken as string
+   */
   setRefreshToken(refreshToken: string): void {
     localStorage.setItem(this.getRefreshTokenName(), refreshToken)
   }
 
+  /**
+   * Get the Refresh Token from storage
+   * @returns {string|undefined} The stored Refresh Token
+   */
   getRefreshToken(): string | undefined {
     return localStorage.getItem(this.getRefreshTokenName()) ?? undefined
   }
 
+  /**
+   * Remove the Refresh Token from storage
+   */
   unsetRefreshToken(): void {
     localStorage.removeItem(this.getRefreshTokenName())
   }
